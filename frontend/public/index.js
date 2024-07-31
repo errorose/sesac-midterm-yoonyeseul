e7ab5816fbe27ce1276e4269b5970fbed6a5663e
errorose <yoonyeseul2000@gmail.com>
2024-07-31 15:16:01 +0900

    exam

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,4 +1,20 @@
-fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(()=>{
-        
-    })
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+const resultBox = document.querySelector('#resultBox');
+
+        async function getTodos(){
+            console.log('btn click');
+
+            try{
+                const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+                console.log('response >> ', response);
+
+                const imgUrl = response.data.message; 
+                const apiImg = `<img src="${imgUrl}" alt="apiImg" />`
+                resultBox.innerHTML = apiImg;
+            }catch(error){
+                console.error(error);
+                resultBox.textContent = '알 수 없는 오류...'
+                resultBox.style.color = 'red'
+            }
+        }
