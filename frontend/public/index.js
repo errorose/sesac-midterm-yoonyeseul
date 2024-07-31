ca8c8cc4f4ac2c83c80bfc1da605f0846f1b4e80
errorose <yoonyeseul2000@gmail.com>
2024-07-31 15:46:04 +0900

    exam

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,20 +1,19 @@
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
 const resultBox = document.querySelector('#resultBox');
 
-        async function getTodos(){
-            console.log('btn click');
+async function getTodos(){
+    console.log('btn click');
 
-            try{
-                const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-                console.log('response >> ', response);
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+        console.log('response >> ', response);
 
-                const imgUrl = response.data.message; 
-                const apiImg = `<img src="${imgUrl}" alt="apiImg" />`
-                resultBox.innerHTML = apiImg;
-            }catch(error){
-                console.error(error);
-                resultBox.textContent = '알 수 없는 오류...'
-                resultBox.style.color = 'red'
-            }
-        }
+        const imgUrl = response.data.message; 
+        const apiTodo = `<input type="checkbox" value="${data}"/>`
+        resultBox.innerHTML = apiTodo;
+    }catch(error){
+        console.error(error);
+        resultBox.textContent = '알 수 없는 오류...'
+        resultBox.style.color = 'red'
+    }
+}
+
